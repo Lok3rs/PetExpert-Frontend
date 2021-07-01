171fa4721853c6ca40c115aa189d88950f6a914b
Lok3rs <lok3rsss@gmail.com>
2021-07-01 13:27:07 +0200

    Finish BaseRegister.js validation

--- src/components/Authentication/BaseRegister.js
+++ src/components/Authentication/BaseRegister.js
@@ -6,14 +6,22 @@ import styles from "./BaseRegister.module.css";
 
 const BaseRegister = () => {
 
-    const passwordField = document.getElementById("passwordField");
-    const confirmPasswordField = document.getElementById("confirmPasswordField");
-    const emailField = document.getElementById('emailField');
 
     const [validEmail, setValidEmail] = useState(true);
     const [validPassword, setValidPassword] = useState(true);
     const [matchingPasswords, setMatchingPasswords] = useState(true);
-    const [registerAllowed, setRegisterAllowed] = useState(false);
+
+    const [enteredFirstName, setEnteredFirstName] = useState('');
+    const [enteredLastName, setEnteredLastName] = useState('');
+    const [enteredEmail, setEnteredEmail] = useState('');
+    const [enteredPassword, setEnteredPassword] = useState('');
+    const [enteredPasswordConfirm, setEnteredPasswordConfirm] = useState('');
+
+    const [firstNameEmpty, setFirstNameEmpty] = useState(false);
+    const [lastNameEmpty, setLastNameEmpty] = useState(false);
+    const [emailEmpty, setEmailEmpty] = useState(false);
+    const [passwordFieldEmpty, setPasswordFieldEmpty] = useState(false);
+    const [passwordConfEmpty, setPasswordConfEmpty] = useState(false);
 
     const errors = {
         invalidEmail:
@@ -25,57 +33,129 @@ const BaseRegister = () => {
                 Hasło musi zawierać przynajmniej 8 znaków w tym jedną małą i wielką literę, cyfrę oraz znak specjalny
             </small>,
         differentPasswords:
-            <small className={styles.invalid}>Podane hasła różnią się od siebie</small>
+            <small className={styles.invalid}>Podane hasła różnią się od siebie</small>,
+        emptyField:
+            <small className={styles.invalid}>To pole nie może być puste.</small>
     }
 
-    const validateEmail = (event) => {
-        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        setValidEmail(re.test(String(event.target.value).toLowerCase()));
+    const changeFirstNameFieldHandler = (event) => {
+        setEnteredFirstName(event.target.value);
+        setFirstNameEmpty(false);
     };
 
-    const validatePassword = (event) => {
+    const changeLastNameFieldHandler = (event) => {
+        setEnteredLastName(event.target.value);
+        setLastNameEmpty(false);
+    };
+
+    const changeEmailFieldHandler = (event) => {
+        setEnteredEmail(event.target.value);
+        setEmailEmpty(false);
+    };
+
+    const changePasswordFieldHandler = (event) => {
+        setEnteredPassword(event.target.value);
+        setPasswordFieldEmpty(false);
+    };
+
+    const changePasswordConfirmFieldHandler = (event) => {
+        setEnteredPasswordConfirm(event.target.value);
+        setPasswordConfEmpty(false);
+    };
+
+    const validateEmail = () => {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        setValidEmail(re.test(String(enteredEmail).toLowerCase()));
+    };
+
+    const validatePassword = () => {
         const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
-        setValidPassword(re.test(String(event.target.value)));
+        setValidPassword(re.test(String(enteredPassword)));
     };
 
     const validateConfirmPassword = () => {
-        setMatchingPasswords(passwordField.value === confirmPasswordField.value);
+        setMatchingPasswords(enteredPassword === enteredPasswordConfirm);
     };
 
-    const allowRegisterHandler = () => {
-        // TODO: MAKE IT WORKING ASYNCHRONOUSLY !!!!
-        setRegisterAllowed(
-            passwordField != null && confirmPasswordField != null && emailField != null &&
-            validEmail && validPassword && matchingPasswords
-        );
-    }
+    const registerHandler = (event) => {
+        // TODO: Check if email already exists in database
+        event.preventDefault();
+        if ([enteredFirstName, enteredLastName, enteredEmail, enteredPassword, enteredPasswordConfirm]
+            .some(it => it.length === 0)) {
+            setFirstNameEmpty(enteredFirstName === '');
+            setLastNameEmpty(enteredLastName === '');
+            setEmailEmpty(enteredEmail === '');
+            setPasswordFieldEmpty(enteredPassword === '');
+            setPasswordConfEmpty(enteredPasswordConfirm === '');
+        } else {
+
+        }
+    };
+
 
 
     return (
-        <Form className={`${styles.registerForm}`} onChange={allowRegisterHandler}>
-            <Form.Group>
+        <Form className={`${styles.registerForm}`}>
+            <Form.Group className={`my-0`}>
                 <Form.Label>Imię</Form.Label>
-                <Form.Control required id={'firstNameField'} type="email" placeholder="Twoje imię"/>
+                <Form.Control
+                    id={'firstNameField'}
+                    type="email"
+                    placeholder="Twoje imię"
+                    onChange={changeFirstNameFieldHandler}
+                    value={enteredFirstName}
+                />
+                {firstNameEmpty && errors.emptyField}
             </Form.Group>
-            <Form.Group>
+            <Form.Group className={`my-0`}>
                 <Form.Label>Nazwisko</Form.Label>
-                <Form.Control required id={'lastNameField'} type="email" placeholder="Twoje nazwisko"/>
+                <Form.Control
+                    id={'lastNameField'}
+                    type="email"
+                    placeholder="Twoje nazwisko"
+                    onChange={changeLastNameFieldHandler}
+                    value={enteredLastName}
+                />
+                {lastNameEmpty && errors.emptyField}
             </Form.Group>
-            <Form.Group>
+            <Form.Group className={`my-0`}>
                 <Form.Label>Email</Form.Label>
-                <Form.Control id={'emailField'} type="email" placeholder="Wprowadź email" onChange={validateEmail}/>
-                {!validEmail && errors.invalidEmail}
+                <Form.Control
+                    id={'emailField'}
+                    type="email"
+                    placeholder="Wprowadź email"
+                    onChange={changeEmailFieldHandler}
+                    onBlur={validateEmail}
+                    value={enteredEmail}
+                />
+                {emailEmpty && errors.emptyField}
+                {(!validEmail && !emailEmpty) && errors.invalidEmail}
             </Form.Group>
-            <Form.Group>
+            <Form.Group className={`my-0`}>
                 <Form.Label>Hasło</Form.Label>
-                <Form.Control id={'passwordField'} type="password" placeholder="Hasło" onChange={validatePassword}/>
-                {!validPassword && errors.unsecurePassword}
+                <Form.Control
+                    id={'passwordField'}
+                    type="password"
+                    placeholder="Hasło"
+                    onBlur={validatePassword}
+                    onChange={changePasswordFieldHandler}
+                    value={enteredPassword}
+                />
+                {passwordFieldEmpty && errors.emptyField}
+                {(!validPassword && !passwordFieldEmpty) && errors.unsecurePassword}
             </Form.Group>
-            <Form.Group className={`mb-0`}>
+            <Form.Group className={`my-0`}>
                 <Form.Label>Powtórz hasło</Form.Label>
-                <Form.Control id={'confirmPasswordField'} type="password" placeholder="Powtórz hasło"
-                              onChange={validateConfirmPassword}/>
-                {!matchingPasswords && errors.differentPasswords}
+                <Form.Control
+                    id={'confirmPasswordField'}
+                    type="password"
+                    placeholder="Powtórz hasło"
+                    onBlur={validateConfirmPassword}
+                    onChange={changePasswordConfirmFieldHandler}
+                    value={enteredPasswordConfirm}
+                />
+                {passwordConfEmpty && errors.emptyField}
+                {(!matchingPasswords && !passwordConfEmpty) && errors.differentPasswords}
             </Form.Group>
             <div className={`mb-1`}>
                 <small>
@@ -84,7 +164,7 @@ const BaseRegister = () => {
                 </small>
             </div>
 
-            <Button variant="primary" type="submit" disabled={!registerAllowed}>
+            <Button variant="primary" type="submit" onClick={registerHandler}>
                 Zarejestruj
             </Button>
         </Form>
