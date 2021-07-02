22be6671a1b3d77403e77fddfb07056dd253a740
Lok3rs <lok3rsss@gmail.com>
2021-07-02 15:53:25 +0200

    Create SideBar component

--- src/components/MainScreen/MainNavbar/MainNavbar.js
+++ src/components/MainScreen/MainNavbar/MainNavbar.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 
-import {Navbar} from "react-bootstrap";
+import {Nav, Navbar} from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faUser, faBars} from "@fortawesome/free-solid-svg-icons";
 
@@ -9,7 +9,13 @@ import logo from './logo_notext.png'
 import styles from './MainNavbar.module.css';
 import AuthModal from "../../Authentication/AuthModal";
 
-const MainNavbar = () => {
+const MainNavbar = (props) => {
+
+    const [showSideMenu, setShowSideMenu] = useState(false);
+
+    const changeSideMenuVisibility = () => {
+        setShowSideMenu(!showSideMenu);
+    };
 
     const [showAuth, setShowAuth] = useState(false);
 
@@ -17,20 +23,35 @@ const MainNavbar = () => {
         setShowAuth(!showAuth);
     }
 
-return (
-    <>
-        {
-            showAuth && (
-                <AuthModal changeVisibility={changeAuthVisibility} title={"Login"}/>
-            )
-        }
-        <Navbar className={`d-flex justify-content-between align-items-center py-1 ${styles.bgTransparent}`}>
-            <FontAwesomeIcon icon={faBars} className={`p-0`}/>
-            <img src={logo} alt="" className={`${styles.navbarLogo}`}/>
-            <FontAwesomeIcon icon={faUser} className={`p-0`} onClick={changeAuthVisibility}/>
-        </Navbar>
-    </>
-)
+    return (
+        <>
+            {
+                showAuth && (
+                    <AuthModal changeVisibility={changeAuthVisibility} title={"Login"}/>
+                )
+            }
+            <Navbar className={`d-flex justify-content-between align-items-center py-1 ${styles.bgTransparent}`}>
+                <FontAwesomeIcon icon={faBars} className={`p-0`} onClick={changeSideMenuVisibility}/>
+                <img src={logo} alt="" className={`${styles.navbarLogo}`}/>
+                <FontAwesomeIcon icon={faUser} className={`p-0`} onClick={changeAuthVisibility}/>
+            </Navbar>
+            <Navbar className={`${styles.sideBar} ${!showSideMenu ? styles.sideBarHidden : undefined} d-block`} id={`SideBar`}>
+                <Nav.Item className={`${styles.category} ${styles.navItem}`}>Usługi</Nav.Item>
+                <Nav.Item className={`${styles.navItem}`}>Behawioryści</Nav.Item>
+                <Nav.Item className={`${styles.navItem}`}>Hotele / Petsitterzy</Nav.Item>
+                <Nav.Item className={`${styles.navItem}`}>Groomerzy</Nav.Item>
+                <Nav.Item className={`${styles.navItem}`}>Weterynarze</Nav.Item>
+                <Nav.Item className={`${styles.category} ${styles.navItem}`}>Twoje konto</Nav.Item>
+                <Nav.Item className={`${styles.navItem}`}>Logowanie</Nav.Item>
+                <Nav.Item className={`${styles.navItem}`}>Rejestracja</Nav.Item>
+                <Nav.Item className={`${styles.category} ${styles.navItem}`}>Pozostałe</Nav.Item>
+                <Nav.Item className={`${styles.navItem}`}>O nas</Nav.Item>
+                <Nav.Item className={`${styles.navItem}`}>FAQ</Nav.Item>
+                <Nav.Item className={`${styles.navItem}`}>Regulamin</Nav.Item>
+                <Nav.Item className={`${styles.navItem}`}>Wyślij zgłoszenie</Nav.Item>
+            </Navbar>
+        </>
+    )
 
 }
 
--- src/components/MainScreen/MainScreen.js
+++ src/components/MainScreen/MainScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MainNavbar from "./MainNavbar/MainNavbar";
 
 import styles from "./MainScreen.module.css";
@@ -6,6 +6,8 @@ import ServicesBar from "./ServicesBar/ServicesBar";
 
 
 const MainScreen = () => {
+
+
     return (
         <div className={styles.wrapper}>
             <div className={`${styles.h80} ${styles.mainContainer}`}>
