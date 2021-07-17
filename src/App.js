8cccb00f38a8104711b789b07c9efbdec6ef16f3
lok3rs <lok3rsss@gmail.com>
2021-07-17 13:37:40 +0200

    Remove test NewService from App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 
 import MainScreen from "./components/MainScreen/MainScreen";
-import NewService from "./components/Services/NewService";
 
 function App() {
   return (
     <div className="App">
         <MainScreen/>
-        <NewService />
     </div>
   );
 }
